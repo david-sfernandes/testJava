ed6afdd3ee8b0f242500ebcbd7de2d174459122c
david-sfernandes <62266135+david-sfernandes@users.noreply.github.com>
2023-08-27 13:21:47 -0300

    Take and use picture

--- src/screens/PictureFormScreen.tsx
+++ src/screens/PictureFormScreen.tsx
@@ -1,32 +1,47 @@
-// @ts-nocheck
 import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
-import { Image } from "react-native";
-import {
-  ImagePickerResponse,
-  launchCamera
-} from "react-native-image-picker";
+import { Image, Text, View } from "react-native";
+import { ImagePickerResponse, launchCamera } from "react-native-image-picker";
+import { colors, fonts, spacing } from "../styles/base";
+import { Button } from "react-native-paper";
 
 export default function PictureFormScreen() {
-  const [image, setImage] = useState<ImagePickerResponse | null>(null);
+  const [image, setImage] = useState<string>("");
   const navigation = useNavigation();
+  //log image
+  console.log(image);
 
   const takePicture = () => {
     launchCamera({ mediaType: "photo" }, (response) => {
       console.log("1");
-      if (response.uri) {
-        setImage(response);
-      }
-      console.log("2");
-      showPicture();
+      console.log(response);
+      setImage(response.assets[0].uri);
     });
   };
 
-  const showPicture = () => {
-    console.log("3");
-    return (
-      <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
-    );
-  };
-  takePicture();
+
+  return (
+    <View style={{ padding: spacing.lg, backgroundColor: colors.black, height: "100%" }}>
+      <Text style={fonts.h1}>Image picker</Text>
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
+      <Button onPress={() => takePicture()} buttonColor="white">Open camera</Button>
+    </View>
+  );
 }
+
+// Camera result example:
+// {
+//   assets: [
+//     {
+//       fileName:
+//         "rn_image_picker_lib_temp_090ef231-f0e1-4b18-b1fd-0c086abbf6ca.jpg",
+//       fileSize: 1002739,
+//       height: 4096,
+//       type: "image/jpeg",
+//       uri: "file:///data/user/0/com.testjava/cache/rn_image_picker_lib_temp_090ef231-f0e1-4b18-b1fd-0c086abbf6ca.jpg",
+//       width: 2304,
+//     },
+//   ],
+// };
